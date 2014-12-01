caa7e37915a7984de1c5ab8f5f9cfd4a918124a3
Taro Hanamura <m@hanamurataro.com>
2014-12-01 17:30:05 +0900

    Block checking core updates

--- src/UpdateBlockHook.php
+++ src/UpdateBlockHook.php
@@ -8,6 +8,8 @@ class UpdateBlockHook
   {
     add_filter('automatic_updater_disabled', array($this, 'onAutomaticUpdaterDisabled'));
     add_filter('pre_site_transient_update_core', array($this, 'onPreSiteTransientUpdateCore'));
+    remove_action('wp_version_check', 'wp_version_check');
+    remove_action('admin_init', '_maybe_update_core');
   }
 
   public function onAutomaticUpdaterDisabled($bool)
